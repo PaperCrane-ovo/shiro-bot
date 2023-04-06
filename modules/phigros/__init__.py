e57fbe02b37dd8fd3e0059226fa7bc5006fb6b93
Crane <15352461680@126.com>
2023-04-06 20:51:59 +0800

    将__init__.py的代码移动到main.py

--- modules/phigros/__init__.py
+++ modules/phigros/__init__.py
@@ -1,29 +1,29 @@
 '''phigros模块的初始化文件，用于导入模块中的类和函数。'''
 
-
 from os.path import dirname
 import shlex
 from typing import Annotated
 from datetime import datetime
 import json5
-
-from graia.saya.builtins.broadcast.schema import ListenerSchema
-from graia.saya import Channel
-from graia.ariadne.model import Group, Member
-from graia.ariadne.message.chain import MessageChain
-from graia.ariadne.message import Source
-from graia.ariadne.event.message import GroupMessage, TempMessage
-from graia.ariadne.message.element import ForwardNode, Forward
-from graia.ariadne.app import Ariadne
 from graia.ariadne.message.parser.base import DetectPrefix
-from .user import User
-from .score import Phigros
-from .pgrapi import PhigrosUnlimitedAPI
+from graia.ariadne.app import Ariadne
+from graia.ariadne.message.element import ForwardNode, Forward
+from graia.ariadne.event.message import GroupMessage, TempMessage
+from graia.ariadne.message import Source
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.model import Group, Member
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from .pgrapi import PhigrosUnlimitedAPI as PhigrosUnlimitedAPI
+from .score import Phigros as Phigros
+from .user import User as User
+from .main import main_phigros as main_phigros
+dir_name = dirname(__file__)
 
 
 channel = Channel.current()
 
-dir_name = dirname(__file__)
+
 with open(f'{dir_name}/config.json', 'r', encoding='utf-8') as f:
     config = json5.load(f)
 pgr_prefix = config['pgr_prefix']  # type: ignore
@@ -63,63 +63,7 @@ async def phigros(bot: Ariadne,
 
     Returns:None
     '''
-
-    # 写了一堆的if-else才发现有更好的匹配器 我是伞兵
-    # TODO: 用匹配器重写
-
-    message_str = message.display.strip()
-    command, *args = shlex.split(message_str)
-
-    match command:
-        case 'bind':
-            #  绑定用户信息
-            if len(args) == 0:
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain([f'请输入session_token!']), quote=source)
-                elif isinstance(message_type, TempMessage):
-                    await bot.send_temp_message(member, MessageChain([f'请输入session_token']), group)
-                return
-            player = User.get_user(member.id)
-            if player is None:
-                player = User(member.id, member.name, args[0])
-                result = player.register()
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain([result]))
-                elif isinstance(message_type, TempMessage):
-                    await bot.send_temp_message(member, MessageChain([result]), group)
-            else:
-                player.session_token = args[0]
-                result = player.update_user_info()
-                result = '用户信息已存在,'+result
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain([result]))
-                elif isinstance(message_type, TempMessage):
-                    await bot.send_temp_message(member, MessageChain([result]), group)
-        case 'b19':
-            # 获取b19数据
-            player = User.get_user(member.id)
-            if player is None:
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain(['请先绑定session_token!']), quote=source)
-                elif isinstance(message_type, TempMessage):
-                    await bot.send_temp_message(member, MessageChain(['请先绑定session_token']), group)
-                return
-            pgr = Phigros(player)
-
-            b19 = await pgr.best19()
-            if '--nf' in args:  # not forward
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain([b19]))
-
-            else:
-                forward = ForwardNode(
-                    target=bot.account,
-                    time=datetime.now(),
-                    message=MessageChain([b19]),  # type: ignore
-                    name='真白鹤甜甜'
-                )
-                if isinstance(message_type, GroupMessage):
-                    await bot.send_group_message(group, MessageChain(Forward([forward])))
+    return await main_phigros(bot, group, member, message, message_type, source)
 
 
 __author__ = 'crane'
---
+++ modules/phigros/main.py
@@ -0,0 +1,85 @@
+'''__init__.py的函数实现.'''
+from datetime import datetime
+import shlex
+from graia.ariadne.entry import Ariadne, Group, Member, MessageChain, GroupMessage, TempMessage, Source, ForwardNode, Forward
+from . import User, Phigros
+# saya的模块导入只对__init__.py有效,所以在之前的版本中将所有的函数都放在__init__.py中.
+# 简化版
+
+
+async def main_phigros(bot: Ariadne,
+                       group: Group,
+                       member: Member,
+                       message: MessageChain,
+                       message_type: GroupMessage | TempMessage,
+                       source: Source):
+    '''
+    监听函数,用于处理phigros相关指令
+
+    Parameters:
+        bot: Ariadne对象
+        group: Group对象
+        member: Member对象
+        message: MessageChain对象
+        message_type: GroupMessage或TempMessage对象
+        source: Source对象
+
+    Returns:None
+    '''
+
+    # 写了一堆的if-else才发现有更好的匹配器 我是伞兵
+    # TODO: 用匹配器重写
+
+    message_str = message.display.strip()
+    command, *args = shlex.split(message_str)
+
+    match command:
+        case 'bind':
+            #  绑定用户信息
+            if len(args) == 0:
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain(['请输入session_token!']), quote=source)
+                elif isinstance(message_type, TempMessage):
+                    await bot.send_temp_message(member, MessageChain(['请输入session_token']), group)
+                return
+            player = User.get_user(member.id)
+            if player is None:
+                player = User(member.id, member.name, args[0])
+                result = player.register()
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain([result]))
+                elif isinstance(message_type, TempMessage):
+                    await bot.send_temp_message(member, MessageChain([result]), group)
+            else:
+                player.session_token = args[0]
+                result = player.update_user_info()
+                result = '用户信息已存在,'+result
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain([result]))
+                elif isinstance(message_type, TempMessage):
+                    await bot.send_temp_message(member, MessageChain([result]), group)
+        case 'b19':
+            # 获取b19数据
+            player = User.get_user(member.id)
+            if player is None:
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain(['请先绑定session_token!']), quote=source)
+                elif isinstance(message_type, TempMessage):
+                    await bot.send_temp_message(member, MessageChain(['请先绑定session_token']), group)
+                return
+            pgr = Phigros(player)
+
+            b19 = await pgr.best19()
+            if '--nf' in args:  # not forward
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain([b19]))
+
+            else:
+                forward = ForwardNode(
+                    target=bot.account,
+                    time=datetime.now(),
+                    message=MessageChain([b19]),  # type: ignore
+                    name='真白鹤甜甜'
+                )
+                if isinstance(message_type, GroupMessage):
+                    await bot.send_group_message(group, MessageChain(Forward([forward])))
