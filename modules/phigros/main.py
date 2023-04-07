3c5a4f91dc5dff0ae5528ae9c88dd2f38ad95448
Crane <15352461680@126.com>
2023-04-08 02:13:23 +0800

    增加了并没有写完的歌曲查询功能

--- modules/phigros/main.py
+++ modules/phigros/main.py
@@ -2,15 +2,21 @@
 from datetime import datetime
 import shlex
 from typing import Annotated
+import json5
 from graia.ariadne.entry import Ariadne, Group, Member, MessageChain, GroupMessage, TempMessage, Source, ForwardNode, Forward, DetectPrefix
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from . import User, Phigros
-from .prefix import prefix
+from .variable import prefix, dir_name, game_info
+
+
 # saya的模块导入只对__init__.py有效,所以在之前的版本中将所有的函数都放在__init__.py中.
 # 简化版
 
 channel = Channel.current()
+
+with open(f'{dir_name}/GameInformation.json', 'r', encoding='utf-8') as f:
+    GameInformation = json5.load(f)
 
 
 @channel.use(ListenerSchema(listening_events=[GroupMessage, TempMessage]))
@@ -90,3 +96,7 @@ async def main_phigros(bot: Ariadne,
                 )
                 if isinstance(message_type, GroupMessage):
                     await bot.send_group_message(group, MessageChain(Forward([forward])))
+
+        case 'song':
+            # 获取歌曲信息
+            pass
--- modules/phigros/variable.py
+++ modules/phigros/variable.py
@@ -1,10 +1,11 @@
-'''Prefixes for the phigros module'''
+'''一些预处理的变量'''
 from os.path import dirname
 import json5
 
+# 当前文件夹的路径
 dir_name = dirname(__file__)
 
-
+# phigros命令前缀
 with open(f'{dir_name}/config.json', 'r', encoding='utf-8') as f:
     config = json5.load(f)
 pgr_prefix = config['pgr_prefix']  # type: ignore
@@ -18,3 +19,7 @@ prefix += pgr_prefix if isinstance(pgr_prefix, list) else [pgr_prefix]
 prefix += [bot_prefix + ' '+p for p in pgr_prefix] if isinstance(
     pgr_prefix, list) else [bot_prefix+' '+pgr_prefix]
 prefix = list(set(prefix))
+
+# 歌曲信息 虽然很大 但不是很大
+with open(f'{dir_name}/GameInformation.json', 'r', encoding='utf-8') as f:
+    game_info = json5.load(f)['song']  # type: ignore
